531747d04de00369dd5c5f46f2c3f696535106a6
SAGAR <guru@insoftnepal.com>
2024-11-10 10:17:21 +0545

    a

--- DataServiceLayer/obj/Debug/netcoreapp3.1/DataServiceLayer.AssemblyInfo.cs
+++ DataServiceLayer/obj/Debug/netcoreapp3.1/DataServiceLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataServiceLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc91def2fb1f6f8d404510a3d955151840c5cd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c44ce0759589a9d7a8c862ee2e4bd69f6ca1c8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataServiceLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataServiceLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
