5279e2e8d499c7b26652bd4d559ba9f242447569
SAGAR <guru@insoftnepal.com>
2024-11-15 15:14:35 +0545

    a

--- DataProvider/obj/Debug/netcoreapp3.1/DataProvider.AssemblyInfo.cs
+++ DataProvider/obj/Debug/netcoreapp3.1/DataProvider.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataProvider")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f915ba0015c14862113b6d35c194f667e19bc10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ff94b7d608c0639f2635e98d9d5edc586fdc50")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataProvider")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataProvider")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
